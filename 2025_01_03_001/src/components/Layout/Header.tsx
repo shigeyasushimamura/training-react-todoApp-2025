80535168f9692216a35f6ef5222e174865fa6104
shigeyasu <sgys.mtk@gmail.com>
2025-01-03 15:34:09 +0900

    cssをbem記法に

--- 2025_01_03_001/src/components/Layout/Header.tsx
+++ 2025_01_03_001/src/components/Layout/Header.tsx
@@ -2,17 +2,17 @@ import styles from "./Layout.module.css";
 
 const Header = () => {
   return (
-    <div className={styles["l-header"]}>
+    <div className={styles["header"]}>
       <header>
-        <h1>
+        <h1 className={styles["header__header"]}>
           <a href="/">タイトル</a>
         </h1>
       </header>
-      <nav>
-        <ul className={styles["nav-container"]}>
-          <li className={styles["nav-item"]}>お知らせ</li>
-          <li className={styles["nav-item"]}>採用情報</li>
-          <li className={styles["nav-item"]}>金融機関の皆様へ</li>
+      <nav className={styles["header__nav"]}>
+        <ul className={styles["header__list"]}>
+          <li className={styles["header__item"]}>お知らせ</li>
+          <li className={styles["header__item"]}>採用情報</li>
+          <li className={styles["header__item"]}>金融機関の皆様へ</li>
         </ul>
       </nav>
     </div>
