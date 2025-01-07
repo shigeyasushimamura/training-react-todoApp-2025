2d30ee35e227570ca19ca4bd29cc3f853b02b13c
shigeyasu <sgys.mtk@gmail.com>
2025-01-07 21:46:57 +0900

    詳細画面

--- 2025_01_04_001/src/features/news/components/DetailNews.tsx
+++ 2025_01_04_001/src/features/news/components/DetailNews.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { News } from "../types/Types";
 import { useState, useEffect } from "react";
+import styles from "./News.module.css";
 
 const DetailNews = ({ newsList }: { newsList: News[] }) => {
   const { id } = useParams<{ id: string }>();
@@ -15,14 +16,21 @@ const DetailNews = ({ newsList }: { newsList: News[] }) => {
   }, [id, newsList]);
 
   return (
-    <div>
-      <h1>詳細画面</h1>
+    <div className={styles["news-detail"]}>
+      <h1 className={styles["news-detail__heading"]}>詳細画面</h1>
       {data && (
-        <article>
-          {data.id}
-          {data.title}
-          {data.content}
-          {data.category}
+        <article className={styles["news-detail__article"]}>
+          <div className={styles["news-detail__image-container"]}>
+            <img src={data.path} className={styles["news-detail__image"]} />
+            <div className={styles["news-detail__category"]}>
+              {data.category}
+            </div>
+          </div>
+
+          <div className={styles["news-detail__article-heading"]}>
+            <h2> {data.title}</h2>
+          </div>
+          <div className={styles["news-detail__content"]}> {data.content}</div>
         </article>
       )}
     </div>
