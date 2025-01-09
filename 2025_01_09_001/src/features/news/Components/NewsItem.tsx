9906c188f14e6a87c5e282608b7dd9bb1eaac1d2
shigeyasu <sgys.mtk@gmail.com>
2025-01-09 17:19:57 +0900

    ワンパス

--- 2025_01_09_001/src/features/news/Components/NewsItem.tsx
+++ 2025_01_09_001/src/features/news/Components/NewsItem.tsx
@@ -1,5 +1,21 @@
-const NewsItem = () => {
-  return <div>NewsItem</div>;
+import { FC } from "react";
+import { News } from "../types/type";
+
+interface Props {
+  news: News;
+}
+
+const NewsItem: FC<Props> = ({ news }) => {
+  return (
+    <div>
+      <header>
+        <h3>NewsItem</h3>
+      </header>
+      <article>
+        <div>{news.title}</div>
+      </article>
+    </div>
+  );
 };
 
 export default NewsItem;
--- 2025_01_09_001/src/features/news/Components/NewsList.tsx
+++ 2025_01_09_001/src/features/news/Components/NewsList.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { News } from "../types/type";
+import NewsItem from "./NewsItem";
 
 interface Props {
   newsList: News[];
@@ -9,6 +10,15 @@ const NewsList: FC<Props> = ({ newsList }) => {
   return (
     <div>
       <h2>NewsList</h2>
+      <ul>
+        {newsList.map((n) => {
+          return (
+            <li key={n.id}>
+              <NewsItem news={n} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
